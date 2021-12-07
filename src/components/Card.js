193e05d59f2bcd0db185abdf38ea6d883b45574c
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-06 22:03:08 -0800

    Added quantity owned state stored in local storage and add/subtract buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Components.css';
 
+import useQuantityOwned from "./hooks/useQuantityOwned";
+
 const Card = (props) => {
     const { card, chosenCards, setChosenCards } = props;
+    const [quantityOwned, setQuantityOwned] = useQuantityOwned();
 
     const handleChosenCardChange = (e) => {
         const tempCards = chosenCards;
@@ -17,17 +20,31 @@ const Card = (props) => {
         }
     }
 
+    const handleAdd = () => {
+        setQuantityOwned(quantityOwned + 1);
+    }
+
+    const handleSubtract = () => {
+        if (quantityOwned > 0) {
+        setQuantityOwned(quantityOwned - 1)
+        }
+    }
+
+
     return (
         <div className='card'>
             <img src={`https://images.digimoncard.io/images/cards/${card.cardnumber}.jpg`} alt={`${card.cardnumber}`} className='card-img' />
             <div className='card-info'>
                 <p>{card.name}</p>
                 <p>{card.cardnumber}</p>
+                <p>Quantity Owned: {quantityOwned}</p>
+                <button onClick={handleAdd}>Add Owned</button>
+                <button onClick={handleSubtract}>Subtract Owned</button>
                 {
-                chosenCards.includes(card.cardnumber) ?                 
-                    <button onClick={handleChosenCardChange} value={true} >Remove From Collection List</button> :
-                    <button onClick={handleChosenCardChange} value={false} >Add to Collection List</button>
-                }
+                !chosenCards.includes(card.cardnumber) ?                 
+                    <button onClick={handleChosenCardChange} value={false} >Add to Collection List</button> :
+                    <button onClick={handleChosenCardChange} value={true} >Remove From Collection List</button>
+                } 
             </div>
         </div>
     )
