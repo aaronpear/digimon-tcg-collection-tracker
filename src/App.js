b10a5db043c2e35514aedebe38a722c64c306da1
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-09 15:40:02 -0800

    .

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import React from 'react';
 import Header from './components/Header';
 import CardList from './components/CardList';
 
-
-
 function App() {
-  // useEffect(() => {
-  //   axios.get('https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon%20Card%20Game&sortdirection=asc')
-  //     .then(response => {
-  //       setCards(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     })
-  // }, [])
-
   return (
     <div className="App">
       <Header />
